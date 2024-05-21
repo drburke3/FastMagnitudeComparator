0fa36b5400b28b2f2954b2f8ec4b5bf2b93b20a9
Daniel Burke <drburke3312@gmail.com>
2024-05-21 11:27:06 -0700

    Update test.py
    Three basic sanity tests for initial run

--- test/test.py
+++ test/test.py
@@ -25,16 +25,23 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
+    # Set the input values for A less than B
     dut.ui_in.value = 20
     dut.uio_in.value = 30
-
-    # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
     assert dut.uo_out.value == 1
+    
+    # Set the input values for A equal to B
+    dut.ui_in.value = 20
+    dut.uio_in.value = 20
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 2
+    
+    # Set the input values for A greater than B
+    dut.ui_in.value = 30
+    dut.uio_in.value = 20
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 4
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
